488fcad1e8fbf7387f1735838b9af5867ac94546
minami.yoshihiko <nannany@hey.jp>
2022-10-23 13:47:21 +0900

    show difference between fixedDelay and fixedRate

--- src/main/java/com/example/demo/tasks/StandardOutputTask.java
+++ src/main/java/com/example/demo/tasks/StandardOutputTask.java
@@ -11,8 +11,15 @@ public class StandardOutputTask {
     Logger logger = LoggerFactory.getLogger(StandardOutputTask.class);
 
     @Scheduled(fixedRate = 5000)
-    public void hello() {
-        logger.info("hello world");
+    public void fixedRate() throws InterruptedException {
+        Thread.sleep(1000);
+        logger.info("fixedRate");
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void fixedDelay() throws InterruptedException {
+        Thread.sleep(1000);
+        logger.info("fixedDelay");
     }
 
 }
