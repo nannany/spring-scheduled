ee313b6b5369cdcba3fc48d21188a28250541c8f
minami.yoshihiko <nannany@hey.jp>
2022-10-23 13:33:27 +0900

    use logger

--- src/main/java/com/example/demo/tasks/StandardOutputTask.java
+++ src/main/java/com/example/demo/tasks/StandardOutputTask.java
@@ -1,14 +1,18 @@
 package com.example.demo.tasks;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StandardOutputTask {
 
+    Logger logger = LoggerFactory.getLogger(StandardOutputTask.class);
+
     @Scheduled(fixedRate = 5000)
     public void hello() {
-        System.out.println("hello world");
+        logger.info("hello world");
     }
 
 }
